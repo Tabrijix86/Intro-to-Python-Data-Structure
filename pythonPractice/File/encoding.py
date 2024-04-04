641c7ab4c90071730f2a1f4dae013064bf086ba4
Tabriji <tabrijiummi@gmail.com>
2024-04-04 23:37:00 +0600

    code

--- pythonPractice/File/encoding.py
+++ pythonPractice/File/encoding.py
@@ -1,3 +1,4 @@
 print("ট".encode("utf-8"))
 "a".encode("utf-8")
 print(list("ä".encode("utf-8")))
+print(list("A".encode("utf-8")))
--- pythonPractice/File/open_file.py
+++ pythonPractice/File/open_file.py
@@ -1,8 +1,9 @@
-f = open("pythonPractice/File/basics.ipynb", "r")  # Let's open this notebook file,
+f = open("/Users/tabriji/Documents/GitHub/Intro-to-Python-Data-Structure/pythonPractice/File/basics.ipynb",
+         "r")  # Let's open this notebook file,
 # which is essentially a text file.
 # So you can open it in a texteditor as well.
 
-for i in range(5):  # And read the first five lines
+for i in range(100):  # And read the first five lines
     line = f.readline()
     print(f"Line {i}: {line}", end="")
 f.close()
