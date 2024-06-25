58e2cddfdbd78d34951ad741ae4c35eb4a8dc54c
Tabriji <tabrijiummi@gmail.com>
2024-06-25 12:05:40 +0600

    fixing

--- pythonPractice/random.py
+++
@@ -1,7 +0,0 @@
-n = 100
-even = [False] * (n + 1)
-for i in range(0, n + 1, 2):
-    even[i] = True
-    print(even[i])
-lists = [1, 1] * 5
-print(lists)
--- pythonPractice/textwrap.py
+++
@@ -1,5 +0,0 @@
-import textwrap
-
-string = "This is a very very very very very long string."
-print(textwrap.wrap(string, 8))
-# ['This is', 'a very', 'very', 'very', 'very', 'very', 'long', 'string.']
