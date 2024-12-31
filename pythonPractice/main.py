0872df03f62534f72e37cfe2f2b8bd550b766056
Tabriji <tabrijiummi@gmail.com>
2024-12-31 18:04:24 +0600

    ok

--- pythonPractice/main.py
+++ pythonPractice/main.py
@@ -1,2 +1,9 @@
-print("Hello World")
-print('Next Line')
+v = "lorem ipsum"
+print(type(v))
+v = 10.0
+print(type(v))
+v = 10.0000001
+print(type(v))
+v = 10.345678976543213456785432456786543234567865432456787654321
+print(type(v))
+print(v)
